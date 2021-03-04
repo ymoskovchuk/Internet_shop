ce4989ee958baa67452c20f40de2e148cb6ef8bc
ymoskovchuk <y.a.moskovchuk@gmail.com>
2021-03-04 22:58:53 +0200

    Added models for laptops and smarthones, added admin

--- newshop/mainapp/models.py
+++ newshop/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -14,6 +15,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    class Meta:
+        abstract = True
 
     category = models.ForeignKey(Category, verbose_name='category', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Naming')
@@ -30,7 +34,9 @@ class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='buyer', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE, related_name='related_products')
-    product = models.ForeignKey(Product, verbose_name='product', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='final price')
 
@@ -59,11 +65,11 @@ class Customer(models.Model):
         return f'Buyer: {self.user.first_name, self.user.last_name}'
 
 
-class Specification(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Nmae of the product for specifications')
-
-    def __str__(self):
-        return f'Specifications for the product {self.name}'
+# class Specification(models.Model):
+#
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     name = models.CharField(max_length=255, verbose_name='Nmae of the product for specifications')
+#
+#     def __str__(self):
+#         return f'Specifications for the product {self.name}'
