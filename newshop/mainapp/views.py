04867cc7c4db3b23aed6e618c15789688984b219
ymoskovchuk <y.a.moskovchuk@gmail.com>
2021-03-25 22:01:14 +0200

    Added cart button and possibility to add products to cart, also on the main page now we can see the quantity of products in cart

--- newshop/mainapp/views.py
+++ newshop/mainapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import Laptop, Smartphone, Category, LatestProducts, Customer, Cart
+from .models import Laptop, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
 from .mxins import CategoryDetailMixin
 
 
 class BaseView(View):
 
     def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
         categories = Category.objects.get_categories_for_left_sidebar()
         products = LatestProducts.objects.get_products_for_main_page(
             'laptop', 'smartphone', with_respect_to='laptop'
@@ -15,12 +19,9 @@ class BaseView(View):
         context = {
             'categories': categories,
             'products': products,
+            'cart': cart,
         }
         return render(request, 'base.html', context)
-
-# def test_view(request):
-#     categories = Category.objects.get_categories_for_left_sidebar()
-#     return render(request, 'base.html', {'categories': categories})
 
 
 class ProductDetailView(CategoryDetailMixin, DetailView):
@@ -39,6 +40,11 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
 
@@ -47,6 +53,26 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model = kwargs.get('ct_model')
+        product_slug = kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.owner,
+            cart=cart,
+            content_type=content_type,
+            object_id=product.id,
+            final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
 
 
 class CartView(View):
