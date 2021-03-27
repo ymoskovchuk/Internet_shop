5019221ee85a2301758732541c6a023a125eb114
ymoskovchuk <y.a.moskovchuk@gmail.com>
2021-03-27 12:26:08 +0200

    Added total sum and quantity of products in cart

--- newshop/mainapp/views.py
+++ newshop/mainapp/views.py
@@ -68,6 +68,7 @@ class AddToCartView(CartMixin, View):
         )
         if created:
             self.cart.products.add(cart_product)
+        self.cart.save()
         return HttpResponseRedirect('/cart/')
 
 
