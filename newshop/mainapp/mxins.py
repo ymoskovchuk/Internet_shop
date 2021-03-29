065548faf59197af1a295b160b2d23170e0f143e
ymoskovchuk <y.a.moskovchuk@gmail.com>
2021-03-30 00:11:14 +0300

    Added products in categories pages

--- newshop/mainapp/mxins.py
+++ newshop/mainapp/mxins.py
@@ -1,12 +1,23 @@
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic import View
 
-from .models import Category, Cart, Customer
+from .models import Category, Cart, Customer, Laptop, Smartphone
 
 
 class CategoryDetailMixin(SingleObjectMixin):
 
+    CATEGORY_SLUG2PRODUCT_MODEL = {
+        'laptops': Laptop,
+        'smartphones': Smartphone
+    }
+
     def get_context_data(self, **kwargs):
+        if isinstance(self.get_object(), Category):
+            model = self.CATEGORY_SLUG2PRODUCT_MODEL[self.get_object().slug]
+            context = super().get_context_data(**kwargs)
+            context['categories'] = Category.objects.get_categories_for_left_sidebar()
+            context['category_products'] = model.objects.all()
+            return context
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.get_categories_for_left_sidebar()
         return context
--- newshop/mainapp/views.py
+++ newshop/mainapp/views.py
@@ -42,6 +42,7 @@ class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['ct_model'] = self.model._meta.model_name
+        context['cart'] = self.cart
         return context
 
 
@@ -52,6 +53,11 @@ class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart'] = self.cart
+        return context
 
 
 class AddToCartView(CartMixin, View):
